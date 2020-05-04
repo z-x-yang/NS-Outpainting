e534385dace4ce907269696bab17e61863addb05
z-x-yang <57658428+z-x-yang@users.noreply.github.com>
2020-05-04 21:09:15 +0800

    Correct a mistake.
    The "train_model.py" should be "eval_model.py"

--- eval.sh
+++ eval.sh
@@ -1,1 +1,1 @@
-python train_model.py --trainset-path /path/to/tf-record-trainset --testset-path /path/to/tf-record-testset --checkpoint-path /path/to/checkpoint
+python eval_model.py --trainset-path /path/to/tf-record-trainset --testset-path /path/to/tf-record-testset --checkpoint-path /path/to/checkpoint
